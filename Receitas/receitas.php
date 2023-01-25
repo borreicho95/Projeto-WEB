81639ece58f36788ecf07faa1e765edb1fb563bc
Gabriel Lopes <79711945+borreicho95@users.noreply.github.com>
2023-01-25 11:08:32 +0000

    Pequeno update
    Update do dia 20 de janeiro nao realizado

--- Receitas/receitas.php
+++ Receitas/receitas.php
@@ -18,6 +18,8 @@ $nameCat = $_GET['nameCat'];
     <?php echo '<title>'.$nameCat.'</title>'; ?>
     <link rel="stylesheet" href="receitas.css" >
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   
 </head>
 <body>
     
@@ -76,8 +78,9 @@ $nameCat = $_GET['nameCat'];
         
 </div>
 
-
-<div></div>
+<div class="receita">
+  <h1>A ver Receitas de <?php echo $nameCat; ?></h1>
+</div>
 
 </body>
 </html>
