8cda82795f0f4f8a38b58bd6bb86184ca7a9c6d2
Gabriel Lopes <79711945+borreicho95@users.noreply.github.com>
2023-01-27 11:11:52 +0000

    Update model.php
    Remoção das funções que vao buscar o user

--- Receitas/model.php
+++ Receitas/model.php
@@ -88,64 +88,6 @@ function receitaExists( $search )
     return $receita;
 }
 
-/* 
-   Verifica se um dado username existe 
-*/ 
-function usernameExists( $username )
-{
-    $conexao = estabelerConexao();
-    $stmt = $conexao->prepare("SELECT username FROM users WHERE username=:username" );
-    $stmt->execute( [ 'username' => $username ] );
-    $username = $stmt->fetchColumn();
-
-    return is_string($username);
-}
-
-/* 
-   Adiciona um novo utilizador 
-*/ 
-function adicionarUser( $username )
-{
-    $conexao = estabelerConexao();
-    $stmt = $conexao->prepare("INSERT INTO users (`username`) VALUES (:username)");
-    $stmt->execute( [ 'username' => $username ] );
-}
-
-/* 
-   Retorna um array com os likes associados a um user 
-*/ 
-function getLikes( $username )
-{
-    $conexao = estabelerConexao();
-    $stmt = $conexao->prepare("SELECT id_receita FROM userlikes WHERE username=:username" );
-    $stmt->execute( [ 'username' => $username ] );
-    $likes = $stmt->fetchAll(PDO::FETCH_COLUMN);
-   
-    return $likes; 
-}
-
-/* 
-   Adiciona um Like aos Likes de um utilizador 
-   cujo 'username' é passado no primeiro parâmetro,
-   e a fotoId passada no segundo parâmetro  
-*/ 
-function adicionarLike($username, $idReceita )
-{
-    $conexao = estabelerConexao();
-    $stmt = $conexao->prepare("INSERT INTO userlikes ( `username`, `id_receita` ) VALUES (:username, :id_receita )");
-    $stmt->execute( [ 'username' => $username, 'id_receita' => $idReceita] );
-}
-
-/* 
-   Remove um Like dos Likes de um utilizador 
-   cujo 'username' é passado no primeiro parâmetro,
-    e a fotoId passada no segundo parâmetro  
-*/ 
-function removerLike($username, $idReceita ) {
-    $conexao = estabelerConexao();
-    $stmt = $conexao->prepare("DELETE FROM userlikes WHERE `username`=:username AND `id_receita`=:id_receita ");
-    $stmt->execute( [ 'username' => $username, 'id_receita' => $idReceita] );
-}
 
 
 
